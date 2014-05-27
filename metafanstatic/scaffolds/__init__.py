724325cb4822dd3893917f1ba73c9c4c03506a14
podhmo <podhmo+altair@beproud.jp>
2014-05-28 00:13:48 +0900

    scaffold

--- metafanstatic/scaffolds/__init__.py
+++ metafanstatic/scaffolds/__init__.py
@@ -4,18 +4,21 @@ from zope.interface import implementer
 from korpokkur.interfaces import IScaffoldTemplate
 import os.path
 
-## see: korpokkur.interfaces:IScaffoldTemplate
+# see: korpokkur.interfaces:IScaffoldTemplate
+
+
 @implementer(IScaffoldTemplate)
 class Package(object):
+
     """fanstatic template for metafanstatic"""
     source_directory = os.path.join(os.path.abspath(os.path.dirname(__file__)), "+package+")
-    ##varname -> description, default
+    # varname -> description, default
     expected_words = {
-        "package": ("package name", "sample"), 
-        "description": ("package description", "-"), 
+        "package": ("package name", "sample"),
+        "description": ("package description", "-"),
         "version": ("version number for project", "0.0")
     }
-    __dro__ = ["korpokkur.scaffolds.pygitignore:Package"] #todo:validation
+    __dro__ = ["korpokkur.scaffolds.pygitignore:Package"]  # todo:validation
     template_engine = "mako"
 
 """
