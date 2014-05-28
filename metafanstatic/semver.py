ed7fb986638b3cc7c9a9334982e14b3ea6e743e6
podhmo <podhmo+altair@beproud.jp>
2014-05-29 00:34:16 +0900

    semver is broken yet

--- metafanstatic/semver.py
+++ metafanstatic/semver.py
@@ -12,7 +12,7 @@ import re
 
 
 class SemverVersion(Version):
-    component_re = re.compile(r'(\d+ | [a-zA-Z]+ | \.)', re.VERBOSE)
+    component_re = re.compile(r'(\d+ | [a-zA-Z\*]+ | \.)', re.VERBOSE)
 
     def normalize(self, x):
         x = x.lstrip("vV= ")
@@ -38,21 +38,33 @@ class SemverVersion(Version):
             return None
 
     def __repr__(self):
-        return "%s (%s)" % (self.__class__.__name__, (self.vstring))
+        return "%s (%s)" % (self.__class__.__name__, (self.version))
 
     def _cmp(self, other):
         if isinstance(other, str):
             other = self.__class__(other)
-        c = 0
         try:
-            for i, (x, y) in enumerate(zip_longest(self.version, other.version)):
+            for c, (x, y) in enumerate(zip_longest(self.version, other.version)):
+                if x == "x" or x == "*":
+                    continue
+                if y == "y" or y == "*":
+                    continue
+
                 if y is None:
-                    if isinstance(self.version[c - 1], int):
+                    if len(self.version) == c + 1 and self.version[-1] == 0:
+                        return 0
+                    elif isinstance(self.version[c - 1], int) and isinstance(y, str):
+                        return 1
+                    elif self.version[c - 1] == "x" or self.version[c - 1] == "*":
                         return -1
                     else:
                         return 1
                 if x is None:
-                    if isinstance(self.version[c - 1], int):
+                    if len(other.version) == c + 1 and other.version[-1] == 0:
+                        return 0
+                    elif isinstance(self.version[c - 1], int) and isinstance(y, str):
+                        return 1
+                    elif self.version[c - 1] == "x" or self.version[c - 1] == "*":
                         return 1
                     else:
                         return -1
@@ -60,12 +72,15 @@ class SemverVersion(Version):
                     return -1
                 if x > y:
                     return 1
-                c += 1
+                if "+" == x or y == "+":
+                    return 0
                 if "-" == x or y == "-":
                     raise TypeError("")
             return 0
         except TypeError:
             for x, y in zip_longest(self.version[c:], other.version[c:]):
+                if "+" == x or y == "+":
+                    return 0
                 if x is None:
                     return -1
                 if y is None:
@@ -85,15 +100,109 @@ class SemverVersion(Version):
                     return 1
             return 0
 
-    def __contains__(self, other):
+    def copy(self):
+        v = SemverVersion(None)
+        v.version = self.version.copy()
+        v.vstring = self.vstring
+        return v
+
+    def verup(self):
+        copied = self.copy()
+        for i, x in enumerate(copied.version):
+            if not isinstance(x, int):
+                v = list(copied.version)
+                v[i - 2] += 1
+                for i in range(len(v) - i + 1):
+                    v.pop()
+                copied.version = v
+                break
+        else:
+            if len(copied.version) == 1:
+                copied.version = [copied.version[-1] + 1]
+            else:
+                v = list(copied.version)
+                v[-2] += 1
+                v[-1] = 0
+                copied.version = v
+        return copied
+
+    def neary_equal(self, other):
+        for x, y in zip_longest(self.version, other.version):
+            if x == y:
+                continue
+            if x == "x" or x == "*":
+                continue
+            if x is None:
+                return True
+            return False
         return True
 
 
-xs = [r"([\*0-9a-zA-Z]+[\.\-])*[\*0-9a-zA-Z]+", "\s*(-|=|>=?|<=?|\|{2})\s*", "~|~>", "\^", "\s+"]
+def ver_range(region, version):
+    parsed = parse(region)
+    n = len(parsed)
+    return ver_range_execute(parsed, n, version)
+
+
+def ver_range_execute(parsed, n, version):
+    print(parsed, n)
+    if n == 0:
+        return True
+    if n == 1:
+        if parsed[0] == "*" or parsed[0] == "||":
+            return True
+        return SemverVersion(parsed[0]).neary_equal(SemverVersion(version))
+    if n == 2:
+        if parsed[1] == "*":
+            return True
+        else:
+            if "~" == parsed[0] or "~>" == parsed[0]:  # xxx
+                v = SemverVersion(parsed[1])
+                print("@", v, "<=", SemverVersion(version), "<", v.verup(), "@")
+                print("@", v <= SemverVersion(version))
+                print(SemverVersion(version) < v.verup(), "@")
+                return v <= SemverVersion(version) < v.verup()
+            elif "^" == parsed[0]:
+                raise Exception((n, parsed))
+            elif "=" == parsed[0]:
+                return SemverVersion(parsed[1]) == SemverVersion(version)
+            elif ">=" == parsed[0]:
+                return SemverVersion(parsed[1]) <= SemverVersion(version)
+            elif ">" == parsed[0]:
+                return SemverVersion(parsed[1]) < SemverVersion(version)
+            elif "<=" == parsed[0]:
+                return SemverVersion(parsed[1]) >= SemverVersion(version)
+            elif "<" == parsed[0]:
+                return SemverVersion(parsed[1]) > SemverVersion(version)
+            raise Exception((n, parsed))
+    if n == 3:
+        if parsed[1] == "-":
+            return SemverVersion(parsed[0]) <= SemverVersion(version) <= SemverVersion(parsed[2])
+        elif parsed[1] == "||":
+            return ver_range_execute([parsed[0]], 1, version) or ver_range_execute([parsed[2]], 1, version)
+        elif parsed[1] == "=":
+            return ver_range_execute([parsed[0]], 1, version) == ver_range_execute([parsed[2]], 1, version)
+        elif parsed[1] == ">=":
+            return ver_range_execute([parsed[0]], 1, version) >= ver_range_execute([parsed[2]], 1, version)
+        elif parsed[1] == ">":
+            return ver_range_execute([parsed[0]], 1, version) > ver_range_execute([parsed[2]], 1, version)
+        elif parsed[1] == "<=":
+            return ver_range_execute([parsed[0]], 1, version) <= ver_range_execute([parsed[2]], 1, version)
+        elif parsed[1] == "<":
+            return ver_range_execute([parsed[0]], 1, version) < ver_range_execute([parsed[2]], 1, version)
+    if n == 4:
+        return ver_range_execute(parsed[:2], 2, version) and ver_range_execute(parsed[2:], 2, version)
+    if n == 5:
+        if parsed[2] == "||":
+            return ver_range_execute(parsed[:2], 2, version) or ver_range_execute(parsed[3:], 2, version)
+    raise Exception((n, parsed))
+
+
+xs = [r"([\*0-9a-zA-Z]+[\.\-])*[\*0-9a-zA-Z]+", "\s*(-|=|>=?|<=?|\|{2})\s*", "~>|~", "\^", "\s+"]
 
 
 def get_sym(scanner, x):
-    return x
+    return x.strip()
 
 scanner = re.Scanner([(x, get_sym) for x in xs])
 
@@ -102,108 +211,8 @@ def parse(expr):
     xs = scanner.scan(expr)
     if xs[1] != "":
         raise Exception((xs[0], xs[1]))
-    return [x.strip() for x in xs[0] if x.strip() != '']
-
-range_cands = [
-    ['1.0.0 - 2.0.0', '1.2.3', True],
-    ['1.0.0', '1.0.0', True],
-    ['>=*', '0.2.4', True],
-    ['', '1.0.0', True],
-    ['*', '1.2.3', True],
-    ['*', 'v1.2.3-foo', True],
-    ['>=1.0.0', '1.0.0', True],
-    ['>=1.0.0', '1.0.1', True],
-    ['>=1.0.0', '1.1.0', True],
-    ['>1.0.0', '1.0.1', True],
-    ['>1.0.0', '1.1.0', True],
-    ['<=2.0.0', '2.0.0', True],
-    ['<=2.0.0', '1.9999.9999', True],
-    ['<=2.0.0', '0.2.9', True],
-    ['<2.0.0', '1.9999.9999', True],
-    ['<2.0.0', '0.2.9', True],
-    ['>= 1.0.0', '1.0.0', True],
-    ['>=  1.0.0', '1.0.1', True],
-    ['>=   1.0.0', '1.1.0', True],
-    ['> 1.0.0', '1.0.1', True],
-    ['>  1.0.0', '1.1.0', True],
-    ['<=   2.0.0', '2.0.0', True],
-    ['<= 2.0.0', '1.9999.9999', True],
-    ['<=  2.0.0', '0.2.9', True],
-    ['<    2.0.0', '1.9999.9999', True],
-    ['<\t2.0.0', '0.2.9', True],
-    ['>=0.1.97', 'v0.1.97', True],
-    ['>=0.1.97', '0.1.97', True],
-    ['0.1.20 || 1.2.4', '1.2.4', True],
-    ['>=0.2.3 || <0.0.1', '0.0.0', True],
-    ['>=0.2.3 || <0.0.1', '0.2.3', True],
-    ['>=0.2.3 || <0.0.1', '0.2.4', True],
-    ['||', '1.3.4', True],
-    ['2.x.x', '2.1.3', True],
-    ['1.2.x', '1.2.3', True],
-    ['1.2.x || 2.x', '2.1.3', True],
-    ['1.2.x || 2.x', '1.2.3', True],
-    ['x', '1.2.3', True],
-    ['2.*.*', '2.1.3', True],
-    ['1.2.*', '1.2.3', True],
-    ['1.2.* || 2.*', '2.1.3', True],
-    ['1.2.* || 2.*', '1.2.3', True],
-    ['*', '1.2.3', True],
-    ['2', '2.1.2', True],
-    ['2.3', '2.3.1', True],
-    ['~2.4', '2.4.0', True],  #  >=2.4.0 <2.5.0
-    ['~2.4', '2.4.5', True],
-    ['~>3.2.1', '3.2.2', True],  #  >=3.2.1 <3.3.0,
-    ['~1', '1.2.3', True],  #  >=1.0.0 <2.0.0
-    ['~>1', '1.2.3', True],
-    ['~> 1', '1.2.3', True],
-    ['~1.0', '1.0.2', True],  #  >=1.0.0 <1.1.0,
-    ['~ 1.0', '1.0.2', True],
-    ['~ 1.0.3', '1.0.12', True],
-    ['>=1', '1.0.0', True],
-    ['>= 1', '1.0.0', True],
-    ['<1.2', '1.1.1', True],
-    ['< 1.2', '1.1.1', True],
-    ['1', '1.0.0beta', True],
-    ['~v0.5.4-pre', '0.5.5', True],
-    ['~v0.5.4-pre', '0.5.4', True],
-    ['=0.7.x', '0.7.2', True],
-    ['>=0.7.x', '0.7.2', True],
-    ['=0.7.x', '0.7.0-asdf', True],
-    ['>=0.7.x', '0.7.0-asdf', True],
-    ['<=0.7.x', '0.6.2', True],
-    ['~1.2.1 >=1.2.3', '1.2.3', True],
-    ['~1.2.1 =1.2.3', '1.2.3', True],
-    ['~1.2.1 1.2.3', '1.2.3', True],
-    ['~1.2.1 >=1.2.3 1.2.3', '1.2.3', True],
-    ['~1.2.1 1.2.3 >=1.2.3', '1.2.3', True],
-    ['~1.2.1 1.2.3', '1.2.3', True],
-    ['>=1.2.1 1.2.3', '1.2.3', True],
-    ['1.2.3 >=1.2.1', '1.2.3', True],
-    ['>=1.2.3 >=1.2.1', '1.2.3', True],
-    ['>=1.2.1 >=1.2.3', '1.2.3', True],
-    ['<=1.2.3', '1.2.3-beta', True],
-    ['>1.2', '1.3.0-beta', True],
-    ['>=1.2', '1.2.8', True],
-    ['^1.2.3', '1.8.1', True],
-    ['^1.2.3', '1.2.3-beta', True],
-    ['^0.1.2', '0.1.2', True],
-    ['^0.1', '0.1.2', True],
-    ['^1.2', '1.4.2', True],
-    ['^1.2 ^1', '1.4.2', True],
-    ['^1.2', '1.2.0-pre', True],
-    ['^1.2.3', '1.2.3-pre', True]
-]
+    return [x for x in xs[0] if x != '']
 
 
-# range_cands = [
-#     ['<=   2.0.0', '2.0.0', True],
-#     ['<= 2.0.0', '1.9999.9999', True],
-#     ['<=  2.0.0', '0.2.9', True],
-#     ['<    2.0.0', '1.9999.9999', True],
-# ]
-
-
-for xs in range_cands:
-    print("[%r, %r]," % (xs[0], parse(xs[0])))
-
-# print(parse('1.0.0 - 2.0.0'))
+# print(SemverVersion("0.7.x") >= SemverVersion("0.7.0-asdf"))
+# print(SemverVersion("0.7.0-addf") <= SemverVersion("0.7.x"))
