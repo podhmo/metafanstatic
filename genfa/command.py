5f302d7d9e69572a353b88c123a630011b702801
podhmo <podhmo+altair@beproud.jp>
2014-06-04 01:08:52 +0900

    loading module

---
+++ genfa/command.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+import logging
+logger = logging.getLogger(__name__)
+import json
+import argparse
+import sys
+import os.path
+import json
+from .loading import Loader, OverrideLoader
+
+def scanning(args):
+    loader = Loader()
+    override = OverrideLoader()
+    create_config = CreateConfigMessageExit()
+    loaders = [override, loader, create_config]
+    result = {}
+
+    def load(f):
+        for loader in loaders:
+            data = loader.load(f, noexception=True)
+            if data is not None:
+                return data
+
+    for f in args.files:
+        name = os.path.basename(f)
+        result[name] = load(f)
+    out(json.dumps(result, indent=2, ensure_ascii=False))
+
+
+def main(sys_args=sys.argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("program")
+    sub_parsers = parser.add_subparsers()
+
+    scan_parser = sub_parsers.add_parser("scan")
+    scan_parser.add_argument("--logging", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
+    scan_parser.add_argument("files", nargs="+")
+    scan_parser.set_defaults(logging="DEBUG", func=scanning)
+
+    args = parser.parse_args(sys_args)
+    try:
+        func = args.func
+    except AttributeError:
+        parser.error("unknown action")
+    return func(args)
--- genfa/loading.py
+++ genfa/loading.py
@@ -1,16 +1,22 @@
 # -*- coding:utf-8 -*-
 import logging
 logger = logging.getLogger(__name__)
+import sys
 import os.path
 import json
 
 
+class TargetNotFound(Exception):
+    pass
+
+
 def find_target_from_candidates(root, targets):
-    for f in targets:
-        path = os.path.join(root, f)
-        if os.path.exists(path):
-            return path
-    raise Exception("{path} is not found".format(path=path))
+    for r, ds, fs in os.walk(root):
+        for f in fs:
+            for target in targets:
+                if f == target:
+                    return os.path.join(r, f)
+    raise TargetNotFound("bower.json")
 
 
 def load_from_file(filename):
@@ -18,14 +24,43 @@ def load_from_file(filename):
         return json.load(rf)
 
 
+def pack_from_jsondict(jsondict, path):
+    result = {}
+    result["bower_directory"] = os.path.dirname(path)
+    result["name"] = jsondict["name"]
+    result["version"] = jsondict.get("version")
+    result["dependencies"] = jsondict.get("dependencies", [])
+    result["main"] = jsondict["main"]
+    return result
+
+
+class OverrideLoader(object):
+    def __init__(self,
+                 target_suffix="bower.json",
+                 loader=load_from_file,
+                 packer=pack_from_jsondict,
+                 ):
+        self.loader = loader
+        self.packer = packer
+        self.target_suffix = target_suffix
+
+    def load(self, root, noexception=True):
+        filepath = "./{}.{}".format(os.path.basename(root), self.target_suffix)
+        if not os.path.exists(filepath):
+            return None
+        return self.packer(self.loader(filepath), root)
+
+
 class Loader(object):
     def __init__(self,
                  finder=find_target_from_candidates,
                  loader=load_from_file,
+                 packer=pack_from_jsondict,
                  targets=["bower.json", "complement.json"]):
         self.finder = finder
         self.loader = loader
         self.targets = targets
+        self.packer = pack_from_jsondict
 
     def find_target(self, root):
         return self.finder(root, self.targets)
@@ -34,9 +69,41 @@ class Loader(object):
         logger.info("loading: %s", path)
         return self.loader(path)
 
-    def load(self, root):
-        path = self.find_target(root)
-        data = self.load_from_target(path)
-        data["bower_directory"] = os.path.dirname(path)
-        return data
+    def load(self, root, noexception=False):
+        try:
+            path = self.find_target(root)
+            data = self.load_from_target(path)
+        except TargetNotFound:
+            if noexception:
+                return None
+            raise
+        return self.packer(data, path)
+
+
+def err(message):
+    sys.stderr.write(message)
+    sys.stderr.write("\n")
+    sys.stderr.flush()
+
+
+def out(message):
+    sys.stderr.write(message)
+    sys.stderr.write("\n")
+    sys.stderr.flush()
+
+
+class CreateConfigMessageExit(object):
+    def load(self, f, noexception=True):
+        name = os.path.basename(f)
+        err("please create config file such as below filename:./{}.bower.json".format(name))
+        params = {
+            "bower_directory": f,
+            "name": name,
+            "version": "",
+            "description": "",
+            "main": ["{}.js".format(name)],
+            "dependencies": []
+        }
+        out(json.dumps(params, indent=2, ensure_ascii=False))
+        sys.exit(-1)
 
